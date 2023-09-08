2b7d1f70d93de2840e74c36033edb1283af49c1a
tomasferreras <tomasferreraswork@gmail.com>
2023-09-08 13:56:31 -0300

    feat: add logo, nav

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.navbar}>
-        <a href="/labs/magnetic-button">magnetic button</a>
-        <a href="/labs/text-reveal">text reveal</a>
+        <h1>TUXDI LAB 🧪</h1>
+        <div className={styles.navbarLinks}>
+          <a href="/labs/magnetic-button">magnetic button</a>
+          <a href="/labs/text-reveal">text reveal</a>
+        </div>
       </div>
     </main>
   );
