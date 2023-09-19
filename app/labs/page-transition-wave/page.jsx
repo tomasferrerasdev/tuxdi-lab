41325b2c65c0c2afc6557c2787e376402bbc77ca
tomasferreras <tomasferreraswork@gmail.com>
2023-09-19 15:51:08 -0300

    reset modules

--- app/labs/page-transition-wave/page.jsx
+++ app/labs/page-transition-wave/page.jsx
@@ -8,16 +8,11 @@ const PageTransitionWave = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      const locomotiveScroll = new LocomotiveScroll();
-
-      setTimeout(() => {
-        setIsLoading(false);
-        document.body.style.cursor = "default";
-        window.scrollTo(0, 0);
-      }, 2000);
-    })();
+    setTimeout(() => {
+      setIsLoading(false);
+      document.body.style.cursor = "default";
+      window.scrollTo(0, 0);
+    }, 2000);
   }, []);
 
   return (
